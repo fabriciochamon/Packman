01d569065a78994e46844666977fade2cae187f4
Fabricio Chamon <fabricio.chamon@gmail.com>
2024-07-28 12:21:19 -0300

    first_release

---
+++ packman/main.py
@@ -0,0 +1,463 @@
+import dearpygui.dearpygui as dpg
+import utils, os
+from pprint import pprint
+
+dpg.create_context()
+
+# init packman
+utils.init_packman_user_folder()
+prefs = utils.load_prefs()
+glb = {}
+
+# --------- CALLBACKS --------- #
+
+# toggle off all buttons within "add project" window
+def toggle_off_all_buttons(name_prefix):
+	index = 0
+	while dpg.does_item_exist(f'{name_prefix}--{index}'):
+		tag = f'{name_prefix}--{index}'
+		dpg.bind_item_theme(tag, 'toogle_OFF')
+		index += 1
+
+# handles single/multi-selection toggles within "add project" window
+def project_set_item(sender, app_data, user_data):
+	global glb
+	k = user_data[0]
+	v = user_data[1]
+
+	# handles multi-selection toogles
+	if len(user_data) > 2 and user_data[2]:
+
+		if k in glb.keys():
+
+			isOFF = v not in glb[k]
+
+			if isOFF:
+				glb[k].append(v)
+				dpg.bind_item_theme(sender, 'toogle_ON')
+			else:
+				try:
+					del glb[k][glb[k].index(v)]
+				except:
+					pass
+				dpg.bind_item_theme(sender, 'toogle_OFF')
+		else:
+			glb[k] = [v]
+			dpg.bind_item_theme(sender, 'toogle_ON')
+
+	# handles single-selection toogles
+	else:
+		glb[k] = v
+		prefix = sender.split('--')[0]
+		toggle_off_all_buttons(prefix)
+		dpg.bind_item_theme(sender, 'toogle_ON')
+
+# save package repo in prefs.json upon folder selection
+def set_packages_repo(sender, app_data):
+	global prefs
+
+	folder = app_data['file_path_name']
+	prefs['package_repo'] = folder
+	utils.save_prefs(prefs)
+	dpg.hide_item('package_repo_fb')
+	dpg.set_value('package_repo_tooltip', f'Currently set to: \n{folder}')
+	utils.show_status(f'Package repository set to: {folder}', color=(247, 207, 2))
+
+# show add project window
+def show_project_window(sender=None, app_data=None, user_data=None, edit=False, config=None):
+	global glb
+	glb['edit'] = edit
+	button_groups=['houdini_version','houdini_product','packages']
+
+	if not edit:
+		dpg.configure_item('project_name', enabled=True)
+		dpg.bind_item_theme('project_name', None)
+		for btn_grp in button_groups:
+			toggle_off_all_buttons(btn_grp)
+		dpg.set_value('project_name', 'new project')
+		if 'name' in glb.keys(): del glb['name']
+		if 'houdini_version' in glb.keys(): del glb['houdini_version']
+		if 'houdini_product' in glb.keys(): del glb['houdini_product']
+		if 'packages' in glb.keys(): del glb['packages']
+		glb['archived'] = False
+		dpg.focus_item('project_name')
+	else:
+		dpg.configure_item('project_name', enabled=False)
+		dpg.bind_item_theme('project_name', 'disabled_input_text')
+		glb['name'] = config['name']
+		glb['houdini_version'] = config['houdini_version']
+		glb['houdini_product'] = config['houdini_product']
+		glb['packages'] = config['packages']
+		glb['archived'] = config['archived']
+		dpg.set_value('project_name', config['name'])
+		for btn_grp in button_groups:
+			toggle_off_all_buttons(btn_grp)
+			buttons = dpg.get_item_children(f'grp_{btn_grp}')[1]
+			for btn in buttons:
+				value = dpg.get_item_user_data(btn)[1]
+				if value in glb[btn_grp]:
+					dpg.bind_item_theme(btn, 'toogle_ON')
+		
+	dpg.show_item('add_project_win')
+	size_item('add_project_win', 0.8, 0.8)
+	center_item('add_project_win')
+
+# saves new project configuration
+def save_config():
+	global glb, prefs
+	glb['name'] = dpg.get_value('project_name')
+	validation = utils.validate_new_project(glb)
+	if validation['success']:
+		utils.save_config(glb, prefs)
+		dpg.hide_item('add_project_win')
+		rebuild_config_list()
+		status = 'created' if not glb['edit'] else 'edited successfully'
+		utils.show_status(f'Project \"{glb["name"]}\" {status}.')
+	else:
+		dpg.set_value('ed_message', validation['message'])
+		dpg.show_item('ed')
+		center_item('ed')
+
+# houdini loader for current clicked config
+def load_config(sender, app_data, user_data):
+	print(user_data)
+
+# center widget on main window
+def center_item(tag, offsetX=0, offsetH=0):
+	dpg.split_frame()
+	dpg.split_frame()
+	w = dpg.get_item_rect_size(tag)[0]
+	h = dpg.get_item_rect_size(tag)[1]
+	pw = dpg.get_item_rect_size('mainwin')[0]
+	ph = dpg.get_item_rect_size('mainwin')[1]
+	posx = (pw-w)/2
+	posy = (ph-h)/2
+	dpg.set_item_pos(tag, (posx+offsetX, posy+offsetH))
+
+# resize widget to fit X% of parent's size
+def size_item(tag, percentW, percentH):
+	dpg.split_frame()
+	dpg.split_frame()
+	pw = dpg.get_item_rect_size('mainwin')[0]
+	ph = dpg.get_item_rect_size('mainwin')[1]
+	dpg.set_item_width(tag, pw*percentW)
+	dpg.set_item_height(tag, ph*percentH)
+
+# display confirmation dialog for archiving/deleting configs
+def show_confirmation_dialog(config, action_name):
+	name = config['name']
+	config['do_action'] = True
+	callbacks={
+		'archive':   config_archive,
+		'unarchive': config_unarchive, 
+		'delete':    config_delete,
+	}
+	dpg.set_value('cd_message', f'Do you really want to {action_name} "{name}" ?')
+	dpg.set_item_user_data('cd_btn_ok', config)
+	dpg.set_item_callback('cd_btn_ok', callbacks[action_name])
+	dpg.show_item('cd')
+	center_item('cd')
+
+# edit current config
+def config_edit(sender, app_data, user_data):
+	show_project_window(edit=True, config=user_data)
+
+# archive current config
+def config_archive(sender, app_data, user_data):
+	config = user_data.copy()
+	if 'do_action' in config.keys() and config['do_action']:
+		del config['do_action']
+		dpg.hide_item('cd')
+		utils.archive_config(config)
+		rebuild_config_list()
+		utils.show_status(f'Project \"{config["name"]}\" was archived.')
+	else:
+		show_confirmation_dialog(config, 'archive')
+
+# unarchive current config
+def config_unarchive(sender, app_data, user_data):
+	config = user_data.copy()
+	if 'do_action' in config.keys() and config['do_action']:
+		del config['do_action']
+		dpg.hide_item('cd')
+		utils.unarchive_config(config)
+		rebuild_config_list()
+		utils.show_status(f'Project \"{config["name"]}\" was restored.')
+	else:
+		show_confirmation_dialog(config, 'unarchive')
+
+# delete current config
+def config_delete(sender, app_data, user_data):
+	config = user_data.copy()
+	if 'do_action' in config.keys() and config['do_action']:
+		del config['do_action']
+		dpg.hide_item('cd')
+		utils.delete_config(config)
+		rebuild_config_list()
+		utils.show_status(f'Project \"{config["name"]}\" was deleted!', color=(245, 78, 78))
+	else:
+		show_confirmation_dialog(config, 'delete')
+
+# refresh main configs list
+def rebuild_config_list():
+	groups = ['all_running_configs', 'all_archived_configs']
+	for grp in groups:
+		archived = grp=='all_archived_configs'
+		dpg.delete_item(grp, children_only=True)
+		configs = utils.load_configs() if not archived else utils.load_archived_configs()
+		for config in configs:
+			name = config['name']
+			with dpg.group(horizontal=True, parent=grp):
+				btn_load = dpg.add_button(label=name, user_data=config, width=-1, callback=load_config)
+				
+				with dpg.popup(btn_load, mousebutton=dpg.mvMouseButton_Right):
+					
+					btn = dpg.add_button(label='Edit', width=100, user_data=config, callback=config_edit)
+					dpg.bind_item_theme(btn, 'config_edit_buttons_theme')
+					dpg.bind_item_font(btn, font_small)
+
+					btn = dpg.add_button(label='Archive' if not archived else 'Unarchive', width=100, user_data=config, callback=config_archive if not archived else config_unarchive)
+					dpg.bind_item_theme(btn, 'config_edit_buttons_theme')
+					dpg.bind_item_font(btn, font_small)
+
+					btn = dpg.add_button(label='Delete', width=100, user_data=config, callback=config_delete)
+					dpg.bind_item_theme(btn, 'config_edit_buttons_theme')
+					dpg.bind_item_font(btn, font_small)
+
+				dpg.bind_item_theme(btn_load, 'big_buttons_theme' if not archived else 'big_buttons_archived_theme')
+				dpg.bind_item_font(btn_load, font_big)
+				with dpg.tooltip(parent=btn_load, delay=0.4):
+					dpg.add_text(utils.format_config_display(config), indent=20)
+
+
+# --------- CALLBACKS --------- #
+
+#dpg.show_style_editor()
+
+# set UI font
+with dpg.font_registry():
+	font_path = utils.get_path('./font/Zain-Regular.ttf')
+	font_small  = dpg.add_font(font_path, 26)
+	font_medium = dpg.add_font(font_path, 30)
+	font_large  = dpg.add_font(font_path, 40)
+	font_big    = dpg.add_font(font_path, 50)
+dpg.bind_font(font_medium)
+
+# load UI images
+with dpg.texture_registry():
+	width, height, channels, data = dpg.load_image(utils.get_path('./images/warning.png'))
+	dpg.add_static_texture(width=width, height=height, default_value=data, tag='img_warning')
+	width, height, channels, data = dpg.load_image(utils.get_path('./images/error.png'))
+	dpg.add_static_texture(width=width, height=height, default_value=data, tag='img_error')
+
+# --- THEMES --- #
+with dpg.theme(tag='big_buttons_theme'):
+	with dpg.theme_component(dpg.mvAll):
+		dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 20, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 30, category=dpg.mvThemeCat_Core)
+
+with dpg.theme(tag='big_buttons_archived_theme'):
+	with dpg.theme_component(dpg.mvAll):
+		dpg.add_theme_color(dpg.mvThemeCol_Text, (100,100,100), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 20, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 30, category=dpg.mvThemeCat_Core)
+
+with dpg.theme(tag='big_buttons_action_theme'):
+	with dpg.theme_component(dpg.mvAll):
+		dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 20, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 30, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, (50, 168, 82), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, (50, 168, 82), category=dpg.mvThemeCat_Core)
+
+with dpg.theme(tag='toogle_OFF'):
+	with dpg.theme_component(dpg.mvAll):
+		dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 20, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 30, category=dpg.mvThemeCat_Core)
+
+with dpg.theme(tag='toogle_ON'):
+	with dpg.theme_component(dpg.mvAll):
+		dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (15, 83, 135), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_color(dpg.mvThemeCol_Button, (15, 83, 135), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 20, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 30, category=dpg.mvThemeCat_Core)
+
+with dpg.theme(tag='config_edit_buttons_theme'):
+	with dpg.theme_component(dpg.mvAll):
+		dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 20, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 5, category=dpg.mvThemeCat_Core)
+		dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, (15, 83, 135), category=dpg.mvThemeCat_Core)
+		dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, (15, 83, 135), category=dpg.mvThemeCat_Core)
+
+with dpg.theme(tag='disabled_input_text'):
+	with dpg.theme_component(dpg.mvInputText, enabled_state=False):
+		dpg.add_theme_color(dpg.mvThemeCol_Text, (120,120,120), category=dpg.mvThemeCat_Core)
+# --- THEMES --- #
+
+# add menubar
+with dpg.viewport_menu_bar() as main_menu:
+	dpg.add_menu_item(label="Add project", callback=show_project_window)
+	dpg.add_menu_item(label="Set packages repo", callback=lambda: dpg.show_item('package_repo_fb'))
+	with dpg.tooltip(parent=dpg.last_item(), delay=0.1):
+		pkgrepo = prefs['package_repo'] if 'package_repo' in prefs.keys() else '[None]'
+		dpg.add_text(f'Currently set to: \n{pkgrepo}', tag='package_repo_tooltip')
+		dpg.bind_item_font(dpg.last_item(), font_small)
+
+# WINDOW: main
+with dpg.window(tag='mainwin'):
+	with dpg.group():
+		dpg.add_spacer(height=30)
+		
+		# Running configs
+		dpg.add_group(tag='all_running_configs')
+		
+		dpg.add_spacer(height=20)
+
+		# Archived configs
+		with dpg.collapsing_header(label='Archived'):
+			dpg.add_group(tag='all_archived_configs')
+			
+
+# WINDOW: add project
+with dpg.window(width=600, height=600, pos=(50,50), show=False, tag='add_project_win', no_title_bar=True):
+	
+	# Project info
+	with dpg.group(horizontal=True):
+		dpg.add_text('Name:')
+		dpg.add_input_text(tag='project_name', default_value='new project')
+
+	dpg.add_spacer(height=10)
+
+	# Houdini version
+	with dpg.collapsing_header(label='Houdini version', default_open=True):
+		houdini_installs = utils.get_houdini_installed_versions()
+		multi_selection = False
+		for i, item in enumerate(houdini_installs):
+			if (i%4==0):
+				grp = dpg.add_group(horizontal=True, tag='grp_houdini_version')
+			btn_item = dpg.add_button(tag=f'houdini_version--{i}', label=f'{item}', user_data=['houdini_version', item, multi_selection], callback=project_set_item, parent=grp)
+			dpg.bind_item_theme(btn_item, 'toogle_OFF')
+
+	dpg.add_spacer(height=10)
+
+	# Houdini product
+	with dpg.collapsing_header(label='Houdini product', default_open=True):
+		houdini_products = ['FX', 'Core', 'Indie']
+		multi_selection = False
+		with dpg.group(horizontal=True, tag='grp_houdini_product'):
+			for i, item in enumerate(houdini_products):
+				btn_item = dpg.add_button(tag=f'houdini_product--{i}', label=f'{item}', user_data=['houdini_product', item, multi_selection], callback=project_set_item)
+				dpg.bind_item_theme(btn_item, 'toogle_OFF')
+
+	dpg.add_spacer(height=10)
+
+	# Selected packages
+	with dpg.collapsing_header(label='Load Packages', default_open=True):
+		available_packages = utils.get_available_packages(prefs)
+		multi_selection = True
+		for i, item in enumerate(available_packages):
+			if (i%3==0):
+				grp = dpg.add_group(horizontal=True, tag='grp_packages')
+			btn_package = dpg.add_button(tag=f'packages--{i}', label=f'{item}', user_data=['packages', item, multi_selection], callback=project_set_item, parent=grp)
+			dpg.bind_item_theme(btn_package, 'toogle_OFF')
+
+	dpg.add_spacer(height=30)
+
+	# Action buttons
+	with dpg.group(horizontal=True):
+		btn_save = dpg.add_button(label=f'Save', width=120, callback=save_config)
+		btn_cancel = dpg.add_button(label=f'Cancel', width=120, callback=lambda:dpg.hide_item('add_project_win'))
+		dpg.bind_item_theme(btn_save, 'big_buttons_action_theme')
+		dpg.bind_item_font(btn_save, font_large)
+		dpg.bind_item_theme(btn_cancel, 'big_buttons_action_theme')
+		dpg.bind_item_font(btn_cancel, font_large)
+
+# WINDOW: confirmation dialog
+with dpg.window(tag='cd', show=False, label='Action confirmation', autosize=True):
+	with dpg.group(horizontal=True):
+		dpg.add_image(texture_tag='img_warning')
+		dpg.add_text('', tag='cd_message')
+	with dpg.group(horizontal=True):
+		dpg.add_button(label='Ok', width=75, tag='cd_btn_ok')
+		dpg.add_button(label='Cancel', width=75, callback=lambda:dpg.hide_item('cd'))
+dpg.bind_item_font('cd', font_small)
+
+# WINDOW: error dialog
+with dpg.window(tag='ed', show=False, label='Error', autosize=True):
+	with dpg.group(horizontal=True):
+		dpg.add_image(texture_tag='img_error')
+		dpg.add_text('', tag='ed_message')
+	with dpg.group(horizontal=True):
+		dpg.add_button(label='Ok', width=75, callback=lambda:dpg.hide_item('ed'))
+dpg.bind_item_font('ed', font_small)
+
+# STATUS TEXT BOX
+dpg.add_text('', color=(255,0,0, 255), tag='info_box', parent='mainwin')
+
+# FILE BROWSER: set packages repo
+fb = dpg.add_file_dialog(
+	label='Choose packages repository...',
+	directory_selector=True, 
+	default_path=os.path.expanduser('~'), 
+	modal=True,
+	show=False, 
+	callback=set_packages_repo, 
+	tag="package_repo_fb",
+	width=450,
+	height=380,
+)
+dpg.bind_item_font(fb, font_small)
+
+# init DPG
+ui = utils.load_ui()
+viewport_w = 800 if not ui else ui['size'][0]
+viewport_h = 800 if not ui else ui['size'][1]
+viewport_x = 100 if not ui else ui['pos'][0]
+viewport_y = 100 if not ui else ui['pos'][1]
+
+
+dpg.create_viewport(
+	title='Packman - Houdini Loader', 
+	width=viewport_w, 
+	height=viewport_h, 
+	x_pos=viewport_x, 
+	y_pos=viewport_y, 
+	disable_close=True)
+
+'''
+small_icon = os.path.abspath('./images/packman_logo_64.png')
+large_icon = os.path.abspath('./images/packman_logo_512.png')
+dpg.set_viewport_small_icon(small_icon)
+dpg.set_viewport_large_icon(large_icon)
+'''
+
+dpg.set_primary_window('mainwin', True)
+dpg.set_exit_callback(callback=utils.save_ui)
+dpg.set_viewport_resize_callback(utils.update_status_pos)
+
+# rebuild config buttons (placed here to avoid popup bug showing at (0,0))
+rebuild_config_list()
+
+# init DPG (continue)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+#dpg.start_dearpygui()
+utils.update_status_pos()
+
+# use render loop to dafe info_box color (alpha)
+while dpg.is_dearpygui_running():
+    fade_speed = 0.75
+    info_box_color = dpg.get_item_configuration('info_box')['color']
+    info_box_alpha = info_box_color[3]*255
+    info_box_alpha -= fade_speed
+    if info_box_alpha<=0: info_box_alpha=0
+    info_box_color = [info_box_color[0]*255, info_box_color[1]*255, info_box_color[2]*255, info_box_alpha]
+    dpg.configure_item('info_box', color=info_box_color)
+    
+    dpg.render_dearpygui_frame()
+
+dpg.destroy_context()
---
+++ packman/utils.py
@@ -0,0 +1,243 @@
+import dearpygui.dearpygui as dpg
+import os, re, json, shutil, sys
+from pathlib import Path
+
+packman_home = os.path.expanduser('~/.packman')
+
+# get a list of houdini installs on current machine
+def get_houdini_installed_versions():
+	programs = os.listdir('/opt')
+	pattern = re.compile(r'hfs(\d{2})\.(\d)\.(\d{3})')
+	installs = []
+	for p in programs:
+		if pattern.match(p): 
+			installs.append(p.replace('hfs', ''))
+
+	return sorted(installs, reverse=True)
+
+# creates a home folder to store program data ('~/.packman')
+def init_packman_user_folder():
+	global packman_home
+	if not os.path.isdir(packman_home):
+		os.makedirs(packman_home)
+
+# save custom data
+def save_prefs(data):
+	global packman_home
+	prefs_file = os.path.join(packman_home, 'prefs.json')
+	with open(prefs_file, 'w') as f:
+		json.dump(data, f, indent=4)
+
+# load custom data
+def load_prefs():
+	global packman_home
+	prefs_file = os.path.join(packman_home, 'prefs.json')
+	prefs = {}
+	if os.path.isfile(prefs_file):
+		with open(prefs_file, 'r') as f:
+			prefs = json.load(f)
+	return prefs
+
+# return available packages from prefs file
+def get_available_packages(prefs):
+	packages = []
+	if 'package_repo' in prefs.keys() and prefs['package_repo'] is not None and prefs['package_repo'] != '':
+		packages = [item for item in os.listdir(prefs['package_repo'])]
+	return sorted(packages)
+
+# validates a new project before saving (clashing names, all options chosen, etc)
+def validate_new_project(glb):
+	ret = {'success': True}
+	message = 'Could not create project due to errors below:\n'
+	configs_folder = os.path.join(packman_home, 'configs')
+	config_file = os.path.join(configs_folder, glb['name']+'.json')
+	config_file = config_file.replace(' ', '_')
+	pattern = re.compile(r'[a-zA-Z0-9\-_\ ]+')
+	if glb['name'].strip()=='':
+		ret['success'] = False
+		message += '- Invalid project name (blank).\n'
+	if not re.fullmatch(pattern, glb['name']):
+		ret['success'] = False
+		message += '- Invalid project name. \n\tallowed chars: \n\t\ta-z \n\t\tA-Z \n\t\t0-9 \n\t\t"-" | "_" | " " (dash, underscore, whitespace).\n'
+	if os.path.isfile(config_file) and not glb['edit']:
+		ret['success'] = False
+		message += '- Config name already exists.\n'
+	if 'houdini_version' not in glb.keys():
+		ret['success'] = False
+		message += '- Houdini version not selected.\n'
+	if 'houdini_product' not in glb.keys():
+		ret['success'] = False
+		message += '- Houdini product not selected.\n'
+
+	ret['message'] = message
+	return ret
+
+# saves a new project data inside packman home
+def save_config(glb, prefs):
+	global packman_home
+
+	archived = glb['archived']
+	config={
+		'name': glb['name'],
+		'archived': archived
+	}
+	delete_config(config)
+
+	if 'packages' not in glb.keys():
+		glb['packages'] = []
+
+	# save .json
+	configs_folder = os.path.join(packman_home, 'configs' if not archived else 'archived')
+	if not os.path.isdir(configs_folder):
+		os.makedirs(configs_folder)
+	config_file = os.path.join(configs_folder, glb['name'].strip()+'.json')
+	config_file = config_file.replace(' ', '_')
+	with open(config_file, 'w') as f:
+		json.dump(glb, f, indent=4)
+
+	# save folder with packages copied
+	package_repo = prefs['package_repo']
+	pkg_folder = glb['name'].strip().replace(' ', '_')
+	pkg_folder = os.path.join(configs_folder, pkg_folder)
+	if not os.path.isdir(pkg_folder):
+		os.makedirs(pkg_folder)
+	
+	for item in glb['packages']:
+		src = os.path.join(package_repo, item)
+		dst = os.path.join(pkg_folder, item)
+		shutil.copyfile(src, dst)
+
+# gets all the saved configs under packman home
+def load_configs():
+	global packman_home
+	configs = []
+	configs_folder = os.path.join(packman_home, 'configs')
+	if os.path.isdir(configs_folder):
+		for item in sorted(os.listdir(configs_folder)):
+			if item.endswith('.json'):
+				with open(os.path.join(configs_folder, item), 'r') as f:
+					config = json.load(f)
+					config['archived'] = False
+					configs.append(config)
+	return configs
+
+# prettify a config json to be displayed in the UI
+def format_config_display(config):
+	ret = ''
+	ret += f'Houdini {config["houdini_version"]}  ({config["houdini_product"]})    '
+	ret += '\nPackages:\n\t' 
+	if len(config['packages']):
+		ret += '- '
+		ret += '\n\t- '.join(config['packages'])
+	else:
+		ret += '(None)'
+
+	return ret
+
+
+# archive config (move to "archived" folder)
+def archive_config(config):
+	global packman_home
+
+	config_name = config['name'].replace(' ', '_')
+	configs_folder = os.path.join(packman_home, 'configs')
+	archived_folder = os.path.join(packman_home, 'archived')
+	
+	if not os.path.isdir(archived_folder):
+		os.makedirs(archived_folder)
+
+	src = os.path.join(configs_folder, config_name+'.json')
+	dst = os.path.join(archived_folder, config_name+'.json')
+	shutil.move(src, dst)
+
+	src = os.path.join(configs_folder, config_name)
+	dst = os.path.join(archived_folder, config_name)
+	shutil.move(src, dst)
+
+# unarchive config (move back to "configs" folder)
+def unarchive_config(config):
+	global packman_home
+
+	config_name = config['name'].replace(' ', '_')
+	configs_folder = os.path.join(packman_home, 'configs')
+	archived_folder = os.path.join(packman_home, 'archived')
+	
+	src = os.path.join(archived_folder, config_name+'.json')
+	dst = os.path.join(configs_folder, config_name+'.json')
+	shutil.move(src, dst)
+
+	src = os.path.join(archived_folder, config_name)
+	dst = os.path.join(configs_folder, config_name)
+	shutil.move(src, dst)
+
+
+# gets all the archived configs under packman home
+def load_archived_configs():
+	global packman_home
+	configs = []
+	configs_folder = os.path.join(packman_home, 'archived')
+	if os.path.isdir(configs_folder):
+		for item in sorted(os.listdir(configs_folder)):
+			if item.endswith('.json'):
+				with open(os.path.join(configs_folder, item), 'r') as f:
+					config = json.load(f)
+					config['archived'] = True
+					configs.append(config)
+	return configs
+
+# delete config (running or archived)
+def delete_config(config):
+	global packman_home
+	config_name = config['name'].replace(' ', '_')
+	subfolder = 'configs' if not config['archived'] else 'archived'
+	del_json = os.path.join(packman_home, subfolder, config_name+'.json')
+	del_folder = os.path.join(packman_home, subfolder, config_name)
+	if os.path.isdir(del_folder):
+		shutil.rmtree(del_folder)
+	if os.path.isfile(del_json):
+		os.remove(del_json)
+
+# save dpg init file (window positions, etc)
+def save_ui():
+	prefs = load_prefs()
+	ui = {
+		'pos': dpg.get_viewport_pos(),
+		'size': (dpg.get_viewport_width(), dpg.get_viewport_height()),
+	}
+	prefs['ui'] = ui
+	save_prefs(prefs)
+	dpg.stop_dearpygui()
+
+# load dpg init file 
+def load_ui():
+	prefs = load_prefs()
+	ui = None
+	if 'ui' in prefs.keys():
+		ui = prefs['ui']
+
+	return ui
+
+# update status text position upon viewport resizing
+def update_status_pos():
+	dpg.set_item_pos('info_box', (dpg.get_viewport_width()-dpg.get_item_rect_size('info_box')[0]-30, dpg.get_viewport_height()-50))
+
+# update status text and color (and reset alpha)
+def show_status(message='', color=(46, 143, 199)):
+	dpg.set_value('info_box', message)
+	dpg.split_frame()
+	dpg.split_frame()
+	update_status_pos()
+	dpg.configure_item('info_box', color=(color[0], color[1], color[2], 255))
+
+# pyinstaller: get path relative to tmp directory (extracted files) if running from binary
+def get_path(relative_path):
+	if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+		bundle_dir = Path(sys._MEIPASS)
+	else:
+		bundle_dir = Path(__file__).parent
+
+	bundle_dir = str(bundle_dir).replace('\\', '/')
+	resource_path = f'{bundle_dir}/{relative_path}'
+	
+	return resource_path
+
